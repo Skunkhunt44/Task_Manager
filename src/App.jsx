7739aa54302904d049d1c296b7aec48e7e800224
kwame Adams <kwameadams77@gmail.com>
2023-10-12 11:34:54 -0400

    task-manager

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <div
+    class=" d-flex align-items-center justify-content-center flex-column"
+   
+  >
+    <h3>Todo App</h3>
+    <div class="d-flex align-items-center mb-3">
+      <div class="form-group mr-3 mb-0">
+        <input
+          type="text"
+          class="form-control"
+          id="formGroupExampleInput"
+          placeholder="Enter a task here"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        type="button"
+        class="btn btn-primary mr-3"
+      >
+        Save
+      </button>
+      <button type="button" class="btn btn-warning" >
+        Get Tasks
+      </button>
+    </div>
+   
+  </div>
     </>
   )
 }
